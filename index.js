3290d88fa3996309e4cd71cd7ae12c03496b213a
ophelie turenne <turenneophelie@gmail.com>
2022-12-01 23:29:57 +0400

    complites dropdown js and modified dropdown positio css

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const menu = document.querySelectorAll('.menu li');
 
 menu.forEach((link) => {
-  const submenu = document.querySelector('sub-menu');
+  const subMenu = link.querySelector('.sub-menu');
   link.addEventListener('click', () => {
     link.classList.toggle('active');
+    subMenu.classList.toggle('active-menu');
   });
 });
